e385491f07ec83b00a458dde7735a09d210e6e6d
Chirila Gabriela <gabrielachirila239@gmail.com>
2024-01-06 03:11:19 +0200

    add tables, make menu, login and register functional

--- client.c
+++ client.c
@@ -14,31 +14,17 @@ extern int errno;
 /* portul de conectare la server*/
 int port;
 
+typedef struct {
+  int success;
+  char message[100];
+} Response;
+
 void register_user(int sd) {
     char username[30];
     char password[30];
 
-    printf("[client] Enter your username: ");
-    scanf("%[^\n]s", username);  // %[^\n]s allows spaces in the input
-    write(sd, username, sizeof(username));
-
-    printf("[client] Enter your password: ");
-    scanf(" %[^\n]s", password);  // %[^\n]s allows spaces in the input
-    write(sd, password, sizeof(password));
-
-    Response response;
-    read(sd, &response, sizeof(Response));
-
-    if (response.success) {
-        printf("[client] Registration successful\n");
-    } else {
-        printf("[client] Registration failed: %s\n", response.message);
-    }
-}
-
-void login_user(int sd, int ok) {
-    char username[30];
-    char password[30];
+    bzero(username, 30);
+    bzero(password, 30);
 
     printf("[client] Enter your username: ");
     scanf("%s", username);
@@ -49,50 +35,87 @@ void login_user(int sd, int ok) {
     write(sd, password, sizeof(password));
 
     Response response;
-    read(sd, &response, sizeof(Response));
+    read(sd, &response, sizeof(response));
+
+    if (response.success) {
+        printf("[client] Registration successful\n");
+    } else {
+        printf("[client] Registration failed: %s\n", response.message);
+    }
+}
+
+int login_user(int sd) {
+    char username[30];
+    char password[30];
+
+    bzero(username, 30);
+    bzero(password, 30);
+
+    printf("[client] Enter your username: ");
+    scanf("%s", username);
+    write(sd, username, sizeof(username));
+
+    printf("[client] Enter your password: ");
+    scanf("%s", password);
+    write(sd, password, sizeof(password));
+
+    Response response;
+    read(sd, &response, sizeof(response));
 
     if (response.success) {
         printf("[client] Login successful\n");
-        ok = 1;
+        return 1;
     } else {
         printf("[client] Login failed: %s\n", response.message);
+        return 0;
     }
 }
 
 void menu_after_login(int sd) {
     int option1;
 
-    do {
-        printf ("Acum sunteti autentificat!\n");
-        printf("[client] Choose an option:\n");
-        printf("1. Find nearest parking spot\n");
-        printf("2. View parking history\n");
-        printf("3. View parking availability\n");
-        printf("2. Logout\n");
+    printf ("Acum sunteti autentificat!\n");
 
-        scanf("%d", &option1);
-        write(sd, &option1, sizeof(int));
+    while(1) {
 
-        switch (option1) {
-            case 1:
-                // Implement logout functionality
-                printf("[client] Logout successful\n");
-                break;
-            case 2:
-                // Implement see parking spots functionality
-                printf("[client] Showing parking spots...\n");
-                break;
-            default:
-                printf("[client] Invalid option. Please try again.\n");
-        }
-    } while (option != 3);
+      printf("[client] Choose an option:\n");
+      printf("1. View parking availability\n");
+      printf("2. View parking history\n");
+      printf("3. Find nearest parking spot\n");
+      printf("4. Logout\n");
+
+      scanf("%d", &option1);
+
+      write(sd, &option1, sizeof(int));
+
+      if (option1 == 1) {
+        printf("[client] View parking availability\n");
+      }
+      else if (option1 == 2){
+        printf("[client]  View parking history...\n");
+      }
+      else if (option1 == 3)
+      {
+        printf("[client] Find nearest parking spot...\n");
+      }
+      else if (option1 == 4) 
+      {
+        printf("[client] Logout...\n");
+        break;
+      }
+      else
+      {
+        printf("Invalid option \n");
+      }
+
+  }
 }
 
 int main (int argc, char *argv[])
 {
   int sd;			            // descriptorul de socket
   struct sockaddr_in server;	// structura folosita pentru conectare 
-  char buf[10];
+  char msg[200]=" ";
 
   /* exista toate argumentele in linia de comanda? */
   if (argc != 3)
@@ -144,8 +167,7 @@ int main (int argc, char *argv[])
       register_user(sd);
   }
   else if(option == 2){
-      int ok = 0;
-      login_user(sd, ok);
+      int ok = login_user(sd);
       if (ok == 1) {
           // Call the menu function after a successful login
           menu_after_login(sd);
@@ -153,8 +175,26 @@ int main (int argc, char *argv[])
   }
   else if (option == 3) //iesire din aplicatie
   {
-    printf ("Iesire din aplicatie\n");
+    bzero(msg,200);
+    if (read (sd, msg, 200) < 0)
+    {
+      perror ("[client]Eroare la read() de la server.\n");
+      return errno;
+    }
+    printf ("[client]Mesajul primit este: %s\n", msg);
+    // printf ("Iesire din aplicatie\n");
     break;
+  }
+  else {
+    bzero(msg,200);
+    if (read (sd, msg, 200) < 0)
+    {
+      perror ("[client]Eroare la read() de la server.\n");
+      return errno;
+    }
+    /* afisam mesajul primit */
+    printf ("[client]Mesajul primit este: %s\n", msg);
+    // printf ("Invalid option. Choose a valid option\n");
   }
 
   }
--- server.c
+++ server.c
@@ -35,9 +35,12 @@ typedef struct thData{
 static void *treat(void *); /* functia executata de fiecare thread ce realizeaza comunicarea cu clientii */
 void raspunde(void *);
 
+void createTables(sqlite3 *db);
+
 void register_user(thData tdL, sqlite3 *db);
-void login_user(thData tdL, sqlite3 *db);
+int login_user(thData tdL, sqlite3 *db);
 int verify_username(char username[30],  sqlite3 *db);
+void viewParkingAvailability(thData tdL, sqlite3 *db);
 
 typedef struct {
   int success;
@@ -51,7 +54,7 @@ sqlite3 *open_database_connection() {
   sqlite3 *db;
   int rc;
 
-  rc = sqlite3_open("proiect.db", &db);
+  rc = sqlite3_open("proiect-RC.db", &db);
 
   if (rc) {
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
@@ -86,6 +89,8 @@ int main ()
     sqlite3_free(error_message);
     return -1;
   }
+
+  createTables(db);
 
   close_database_connection(db);
 
@@ -177,26 +182,106 @@ void raspunde(void *arg)
   int option, i=0;
 	struct thData tdL; 
 	tdL= *((struct thData*)arg);
-	if (read (tdL.cl, &option, sizeof(int)) <= 0)
+
+  // Open the database connection
+    sqlite3 *db = open_database_connection();
+    char msgrasp[200]=" ";
+
+    while(1) {
+      
+      	if (read (tdL.cl, &option, sizeof(int)) <= 0)
 			{
 			  printf("[Thread %d]\n",tdL.idThread);
 			  perror ("Eroare la read() de la client.\n");
 			
 			}
 	
-	printf ("[Thread %d]Mesajul a fost receptionat...Optiunea %d\n",tdL.idThread, option);
+      printf ("[Thread %d]Mesajul a fost receptionat...Optiunea %d\n",tdL.idThread, option);
+      // Process the client request based on the received option
+      if (option == 1) {
+          printf("Optiunea 1 \n");
+          register_user(tdL, db);
+      } \
+      else if (option == 2) {
+          printf("Optiunea 2 \n");
+          int ok = login_user(tdL, db);
 
-  // Open the database connection
-    sqlite3 *db = open_database_connection();
+          if (ok == 1) {
+            int option1;
 
-    // Process the client request based on the received option
-    if (option == 1) {
-        printf("Optiunea 1 \n");
-        register_user(tdL, db);
-    } else if (option == 2) {
-        printf("Optiunea 2 \n");
-        login_user(tdL, db);
+            while(1) {
+
+              // dupa ce autentificarea s-a realizat cu succes, trimitem informatii dupa logare
+              if (read (tdL.cl, &option1, sizeof(int)) <= 0)
+              {
+                printf("[Thread %d]\n",tdL.idThread);
+                perror ("Eroare la read() de la client.\n");
+              
+              }
+
+              if (option1 == 1)
+              {
+                printf("[Thread %d] Optiunea %d: View parking availability \n",tdL.idThread, option1);
+                // viewParkingAvailability(tdl, db);
+              }
+              else if (option1 == 2)
+              {
+                printf("[Thread %d] Optiunea %d: View parking history \n",tdL.idThread, option1);
+              }
+              else if (option1 == 3)
+              {
+                printf("[Thread %d] Optiunea %d: Find nearest parking spot \n",tdL.idThread, option1);  
+              }
+              else if(option1 == 4)
+              {
+                printf("[Thread %d] Optiunea %d: Logout \n",tdL.idThread, option1);
+                break;
+              }
+              else 
+              {
+                printf("[Thread %d] Optiunea %d: Optiune invalida \n",tdL.idThread, option1);
+              }
+
+            }
+          }
+      }
+      else if (option == 3) {
+          printf("Optiunea 3 \n");
+          printf("[Thread %d] Clientul a iesit din aplicatie\n", tdL.idThread);
+
+            /*pregatim mesajul de raspuns */
+          bzero(msgrasp,200);
+          strcat(msgrasp,"Clientul a iesit din aplicatie\n");
+          printf("Mesajul de raspuns este: %s", msgrasp);
+
+          if (write (tdL.cl , msgrasp, 200) <= 0)
+          {
+            printf("[Thread %d] ",tdL.idThread);
+            perror ("[Thread]Eroare la write() catre client.\n");
+          }
+          else
+            printf ("[Thread %d]Mesajul a fost trasmis cu succes.\n",tdL.idThread);	
+            break;
+
+      }
+      else {
+          printf("[Thread %d] Optiune invalida. Incercati din nou\n", tdL.idThread);
+          /*pregatim mesajul de raspuns */
+          bzero(msgrasp,200);
+          strcat(msgrasp,"Optiune invalida. Incercati din nou\n");
+          printf("Mesajul de raspuns este: %s", msgrasp);
+
+          if (write (tdL.cl , msgrasp, 200) <= 0)
+          {
+            printf("[Thread %d] ",tdL.idThread);
+            perror ("[Thread]Eroare la write() catre client.\n");
+          }
+          else
+            printf ("[Thread %d]Mesajul a fost trasmis cu succes.\n",tdL.idThread);	
+      }
+
     }
+    
 
     // Close the database connection
     close_database_connection(db);
@@ -322,7 +407,8 @@ int verify_username(char username_exists[30], sqlite3 *db)
 }
 
 
-void login_user(thData tdL, sqlite3 *db) {
+int login_user(thData tdL, sqlite3 *db) {
+    int ok = 0;
     char username[30];
     char password[30];
 
@@ -367,6 +453,7 @@ void login_user(thData tdL, sqlite3 *db) {
             if (step == SQLITE_ROW) {
                 response.success = 1;
                 snprintf(response.message, sizeof(response.message), "Autentificarea s-a realizat cu succes!");
+                ok = 1;
             } else {
                 snprintf(response.message, sizeof(response.message), "Username/parola gresita. Incercati din nou!");
             }
@@ -380,5 +467,246 @@ void login_user(thData tdL, sqlite3 *db) {
 
     if (response.success) {
         printf("%s\n", response.message);
+    }
+
+    if (ok == 1)
+      return 1;
+    else
+      return 0;
+}
+
+
+void createTables(sqlite3 *db) {
+    char *error_message = 0;
+
+    // Drop tables if they already exist
+    char *dropCitiesTableQuery = "DROP TABLE IF EXISTS cities;";
+    char *dropAreasTableQuery = "DROP TABLE IF EXISTS areas;";
+    char *dropParkingSpotsTableQuery = "DROP TABLE IF EXISTS parking_spots;";
+
+    if (sqlite3_exec(db, dropParkingSpotsTableQuery, 0, 0, &error_message) != SQLITE_OK ||
+        sqlite3_exec(db, dropAreasTableQuery, 0, 0, &error_message) != SQLITE_OK ||
+        sqlite3_exec(db, dropCitiesTableQuery, 0, 0, &error_message) != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s \n", error_message);
+        sqlite3_free(error_message);
+        return;
+    }
+    // Create cities table
+    char *createCitiesTableQuery = "CREATE TABLE IF NOT EXISTS cities ("
+                                   "    city_id INTEGER PRIMARY KEY,"
+                                   "    city_name TEXT NOT NULL"
+                                   ");";
+    if (sqlite3_exec(db, createCitiesTableQuery, 0, 0, &error_message) != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s \n", error_message);
+        sqlite3_free(error_message);
+        return;
+    }
+
+    // Populate cities table
+char *populateCitiesTableQuery = "INSERT INTO cities (city_name) VALUES"
+                                 "    ('Alba'),"
+                                 "    ('Arad'),"
+                                 "    ('Arges'),"
+                                 "    ('Bacau'),"
+                                 "    ('Bihor'),"
+                                 "    ('Bistrita-Nasaud'),"
+                                 "    ('Botosani'),"
+                                 "    ('Braila'),"
+                                 "    ('Brasov'),"
+                                 "    ('Bucuresti'),"
+                                 "    ('Buzau'),"
+                                 "    ('Calarasi'),"
+                                 "    ('Caras-Severin'),"
+                                 "    ('Cluj'),"
+                                 "    ('Constanta'),"
+                                 "    ('Covasna'),"
+                                 "    ('Dambovita'),"
+                                 "    ('Dolj'),"
+                                 "    ('Galati'),"
+                                 "    ('Giurgiu'),"
+                                 "    ('Gorj'),"
+                                 "    ('Harghita'),"
+                                 "    ('Hunedoara'),"
+                                 "    ('Ialomita'),"
+                                 "    ('Iasi'),"
+                                 "    ('Ilfov'),"
+                                 "    ('Maramures'),"
+                                 "    ('Mehedinti'),"
+                                 "    ('Mures'),"
+                                 "    ('Neamt'),"
+                                 "    ('Olt'),"
+                                 "    ('Prahova'),"
+                                 "    ('Salaj'),"
+                                 "    ('Satu Mare'),"
+                                 "    ('Sibiu'),"
+                                 "    ('Suceava'),"
+                                 "    ('Teleorman'),"
+                                 "    ('Timis'),"
+                                 "    ('Tulcea'),"
+                                 "    ('Valcea'),"
+                                 "    ('Vaslui'),"
+                                 "    ('Vrancea');";
+
+    if (sqlite3_exec(db, populateCitiesTableQuery, 0, 0, &error_message) != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s \n", error_message);
+        sqlite3_free(error_message);
+        return;
+    }
+
+    // Create areas table with additional column for total_spots
+    char *createAreasTableQuery = "CREATE TABLE IF NOT EXISTS areas ("
+                                  "    area_id INTEGER PRIMARY KEY,"
+                                  "    area_name TEXT NOT NULL,"
+                                  "    city_id INTEGER,"
+                                  "    total_spots INTEGER,"
+                                  "    FOREIGN KEY (city_id) REFERENCES cities(city_id)"
+                                  ");";
+    if (sqlite3_exec(db, createAreasTableQuery, 0, 0, &error_message) != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s \n", error_message);
+        sqlite3_free(error_message);
+        return;
+    }
+char *populateAreasTableQuery = "INSERT INTO areas (area_name, city_id, total_spots) VALUES"
+                                // Areas for Alba County
+                                "    ('Alba Mall Area', 1, 120),"
+                                "    ('Alba City Center', 1, 90),"
+                                // Areas for Arad County
+                                "    ('Arad Shopping District', 2, 150),"
+                                "    ('Arad Historic Center', 2, 180),"
+                                // Areas for Arges County
+                                "    ('Arges Downtown', 3, 80),"
+                                "    ('Arges Old Town', 3, 120),"
+                                // Areas for Bacau County
+                                "    ('Bacau City Center', 4, 100),"
+                                "    ('Bacau Coastal Area', 4, 130),"
+                                "    ('Bacau Port Area', 4, 160),"
+                                // Areas for Bihor County
+                                "    ('Bihor Mall Area', 5, 120),"
+                                "    ('Bihor City Center', 5, 90),"
+                                // Areas for Bistrita-Nasaud County
+                                "    ('Bistrita-Nasaud Shopping District', 6, 150),"
+                                "    ('Bistrita-Nasaud Historic Center', 6, 180),"
+                                // Areas for Botosani County
+                                "    ('Botosani Downtown', 7, 80),"
+                                "    ('Botosani Old Town', 7, 120),"
+                                // Areas for Braila County
+                                "    ('Braila City Center', 8, 100),"
+                                "    ('Braila Coastal Area', 8, 130),"
+                                "    ('Braila Port Area', 8, 160),"
+                                // Areas for Brasov County
+                                "    ('Brasov Mall Area', 9, 120),"
+                                "    ('Brasov City Center', 9, 90),"
+                                // Areas for Bucuresti (Bucharest) County
+                                "    ('Bucharest Shopping District', 10, 150),"
+                                "    ('Bucharest Historic Center', 10, 180),"
+                                // Areas for Buzau County
+                                "    ('Buzau Downtown', 11, 80),"
+                                "    ('Buzau Old Town', 11, 120),"
+                                // Areas for Calarasi County
+                                "    ('Calarasi City Center', 12, 100),"
+                                "    ('Calarasi Coastal Area', 12, 130),"
+                                "    ('Calarasi Port Area', 12, 160),"
+                                // Areas for Caras-Severin County
+                                "    ('Caras-Severin Mall Area', 13, 120),"
+                                "    ('Caras-Severin City Center', 13, 90),"
+                                // Areas for Cluj County
+                                "    ('Cluj Shopping District', 14, 110),"
+                                "    ('Cluj Historic Center', 14, 40),"
+                                // Areas for Constanta County
+                                "    ('Constanta Downtown', 15, 70),"
+                                "    ('Constanta Old Town', 15, 90),"
+                                "    ('Constanta City Center', 15, 140),"
+                                "    ('Constanta Coastal Area', 15, 100),"
+                                "    ('Constanta Port Area', 15, 15),"
+                                // Areas for Covasna County
+                                "    ('Covasna Mall Area', 16, 180),"
+                                "    ('Covasna City Center', 16, 150),"
+                                // Areas for Dambovita County
+                                "    ('Dambovita Shopping District', 17, 160),"
+                                "    ('Dambovita Historic Center', 17, 120),"
+                                // Areas for Dolj County
+                                "    ('Dolj Downtown', 18, 90),"
+                                "    ('Dolj Old Town', 18, 100),"
+                                // Areas for Galati County
+                                "    ('Galati City Center', 19, 170),"
+                                "    ('Galati Coastal Area', 19, 110),"
+                                "    ('Galati Port Area', 19, 90),"
+                                // Areas for Giurgiu County
+                                "    ('Giurgiu Mall Area', 20, 100),"
+                                "    ('Giurgiu City Center', 20, 120),"
+                                // Areas for Gorj County
+                                "    ('Gorj Mall Area', 21, 140),"
+                                "    ('Gorj City Center', 21, 120),"
+                                // Areas for Harghita County
+                                "    ('Harghita Shopping District', 22, 140),"
+                                "    ('Harghita Historic Center', 22, 80),"
+                                // Areas for Hunedoara County
+                                "    ('Hunedoara Downtown', 23, 100),"
+                                "    ('Hunedoara Old Town', 23, 90),"
+                                // Areas for Ialomita County
+                                "    ('Ialomita City Center', 24, 100),"
+                                "    ('Ialomita Coastal Area', 24, 100),"
+                                "    ('Ialomita Port Area', 24, 100),"
+                                // Areas for Iasi County
+                                "    ('Iasi Mall Area', 25, 100),"
+                                "    ('Iasi City Center', 25, 100),"
+                                // Areas for Ilfov County
+                                "    ('Ilfov Shopping District', 26, 100),"
+                                "    ('Ilfov Historic Center', 26, 100),"
+                                // Areas for Maramures County
+                                "    ('Maramures Mall Area', 27, 100),"
+                                "    ('Maramures City Center', 27, 100),"
+                                // Areas for Mehedinti County
+                                "    ('Mehedinti Shopping District', 28, 100),"
+                                "    ('Mehedinti Historic Center', 28, 100),"
+                                // Areas for Mures County
+                                "    ('Mures Downtown', 29, 100),"
+                                "    ('Mures Old Town', 29, 100),"
+                                // Areas for Neamt County
+                                "    ('Neamt City Center', 30, 100),"
+                                "    ('Neamt Coastal Area', 30, 100),"
+                                "    ('Neamt Port Area', 30, 100),"
+                                // Areas for Olt County
+                                "    ('Olt Mall Area', 31, 100),"
+                                "    ('Olt City Center', 31, 100),"
+                                // Areas for Prahova County
+                                "    ('Prahova Shopping District', 32, 100),"
+                                "    ('Prahova Historic Center', 32, 100),"
+                                // Areas for Salaj County
+                                "    ('Salaj Mall Area', 33, 100),"
+                                "    ('Salaj City Center', 33, 100),"
+                                // Areas for Satu Mare County
+                                "    ('Satu Mare Shopping District', 34, 100),"
+                                "    ('Satu Mare Historic Center', 34, 100),"
+                                // Areas for Sibiu County
+                                "    ('Sibiu Downtown', 35, 100),"
+                                "    ('Sibiu Old Town', 35, 100),"
+                                // Areas for Suceava County
+                                "    ('Suceava City Center', 36, 100),"
+                                "    ('Suceava Coastal Area', 36, 100),"
+                                "    ('Suceava Port Area', 36, 100),"
+                                // Areas for Teleorman County
+                                "    ('Teleorman Mall Area', 37, 100),"
+                                "    ('Teleorman City Center', 37, 100),"
+                                // Areas for Timis County
+                                "    ('Timis Shopping District', 38, 100),"
+                                "    ('Timis Historic Center', 38, 100),"
+                                // Areas for Tulcea County
+                                "    ('Tulcea Downtown', 39, 100),"
+                                "    ('Tulcea Old Town', 39, 100),"
+                                // Areas for Valcea County
+                                "    ('Valcea Mall Area', 40, 100),"
+                                "    ('Valcea City Center', 40, 100),"
+                                // Areas for Vaslui County
+                                "    ('Vaslui Shopping District', 41, 100),"
+                                "    ('Vaslui Historic Center', 41, 100),"
+                                // Areas for Vrancea County
+                                "    ('Vrancea Downtown', 42, 100),"
+                                "    ('Vrancea Old Town', 42, 100);";
+
+    if (sqlite3_exec(db, populateAreasTableQuery, 0, 0, &error_message) != SQLITE_OK) {
+        fprintf(stderr, "aici ares SQL error: %s \n", error_message);
+        sqlite3_free(error_message);
+        return;
     }
 }
